394e544335445559ea59a3162a7c0b81f1a3e3b7
Vaharsolta <vaha-22@mail.ru>
2017-05-26 07:06:42 +0300

    Изменена ссылка на ЧПУ в категориях

--- config/web.php
+++ config/web.php
@@ -45,6 +45,7 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'category/<id:\d+>' =>  'category/view',
             ],
         ],
         
