b048aafda5c0188a6d9c32414056e9d9d01bde6a
Vaharsolta <vaha-22@mail.ru>
2017-05-26 05:20:32 +0300

    Создана модель Категории со связью с продуктами

---
+++ models/Category.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vaharsolta
+ * Date: 26.05.2017
+ * Time: 5:16
+ */
+
+namespace app\models;
+use yii\db\ActiveRecord;
+
+class Category extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    public function getProducts() {
+        return $this->hasMany(Product::className(), ['category_id' => 'id']);
+    }
+
+}
