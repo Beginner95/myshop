d6e1dc43c81b572edf593bbd59a876573b4d5380
Vaharsolta <vaha-22@mail.ru>
2017-05-26 06:39:12 +0300

    Реалзован вывод меню в шаблоне

--- components/menu_tpl/menu.php
+++ components/menu_tpl/menu.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Vaharsolta
- * Date: 26.05.2017
- * Time: 6:05
- */
+<li>
+    <?php if (isset($category['childs'])) : ?>
+        <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $category['name']; ?><span class="caret"></span></a>
+    <?php else: ?>
+    <a href=""><?php echo $category['name']; ?></a>
+    <?php endif; ?>
+    <?php if (isset($category['childs'])) : ?>
+        <ul class="dropdown-menu">
+            <?php echo $this->getMenuHtml($category['childs']); ?>
+        </ul>
+    <?php endif; ?>
+</li>
+
+
+
+                    
