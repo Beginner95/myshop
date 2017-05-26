62347cb7a5a21bfb678e7aab82a4c491b5d90f30
Vaharsolta <vaha-22@mail.ru>
2017-05-26 03:54:33 +0300

    Включил ЧПУ

--- config/web.php
+++ config/web.php
@@ -10,6 +10,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'rY4IWpNnf7yy7frY096UFfG3wI5NyNXZ',
+            'baseUrl'  => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
