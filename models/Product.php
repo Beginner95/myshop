64b6e22f772720b5e71638274f270872d5cd2769
Vaharsolta <vaha-22@mail.ru>
2017-05-26 05:24:56 +0300

    Создал модель Продуктов со связью с категориями

--- models/Product.php
+++ models/Product.php
@@ -7,9 +7,16 @@
  */
 
 namespace app\models;
+use yii\db\ActiveRecord;
 
-
-class Product
+class Product extends ActiveRecord
 {
+    public static function tableName()
+    {
+        return 'product';
+    }
 
+    public function getCategory() {
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
+    }
 }
