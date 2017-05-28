976c5ea4bf10794e17656abd2446e2ddbd5d3448
Vaharsolta <vaha-22@mail.ru>
2017-05-28 15:45:27 +0300

    Изменен вид товара

---
+++ controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vaharsolta
+ * Date: 28.05.2017
+ * Time: 13:17
+ */
+
+namespace app\controllers;
+
+
+class ProductController
+{
+
+}
---
+++ views/product/view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vaharsolta
+ * Date: 28.05.2017
+ * Time: 13:22
+ */
