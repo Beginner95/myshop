5c5881040f9025c9762b0e42e34843255184ecf3
Vaharsolta <vaha-22@mail.ru>
2017-05-28 13:15:54 +0300

    Реализована пагинация и получено имя категории

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use app\models\Category;
 use app\models\Product;
 use Yii;
+use yii\data\Pagination;
 
 class CategoryController extends AppController
 {
@@ -15,10 +16,13 @@ class CategoryController extends AppController
 
     public function actionView($id) {
         $id = Yii::$app->request->get('id');
-        $products = Product::find()->where(['category_id' => $id])->all();
+        //$products = Product::find()->where(['category_id' => $id])->all();
+        $query = Product::find()->where(['category_id' => $id]);
+        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 12, 'forcePageParam' => false, 'pageSizeParam' => false]);
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
         $category = Category::findOne($id);
         $this->setMeta('Магазин ' . $category->name, $category->keywords, $category->description);
-        return $this->render('view', compact('products'));
+        return $this->render('view', compact('products', 'pages', 'category'));
     }
 
 }
