6dea0fa6f25ff2a0c62e233f50240b001ffcf4d0
Vaharsolta <vaha-22@mail.ru>
2017-05-28 15:57:24 +0300

    Добавлены метатеги

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -1,15 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Vaharsolta
- * Date: 28.05.2017
- * Time: 13:17
- */
 
 namespace app\controllers;
+use app\models\Category;
+use app\models\Product;
+use Yii;
 
-
-class ProductController
+class ProductController extends AppController
 {
-
+    public function actionView($id) {
+        $id = Yii::$app->request->get('id');
+        $product = Product::findOne($id);
+        //$product = Product::find()->with('category')->where(['id' => $id]);
+        $this->setMeta('Магазин ' . $product->name, $product->keywords, $product->description);
+        return $this->render('view', compact('product'));
+    }
 }
