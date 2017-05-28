671c8523c004625e81eae8ba44daa769967a9f5e
Vaharsolta <vaha-22@mail.ru>
2017-05-28 11:23:14 +0300

    Реализован метод actionView для показа категорий

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -1,15 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Vaharsolta
- * Date: 27.05.2017
- * Time: 16:58
- */
 
 namespace app\controllers;
 
+use app\models\Category;
+use app\models\Product;
+use Yii;
 
-class CategoryController
+class CategoryController extends AppController
 {
+    public function actionIndex() {
+        $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
+        return $this->render('index', compact('hits'));
+    }
+
+    public function actionView($id) {
+        $id = Yii::$app->request->get('id');
+        $products = Product::find()->where(['category_id' => $id])->all();
+        return $this->render('view', compact('products'));
+    }
 
 }
