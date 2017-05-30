bdd76ab4f36dbc2f496cbf5d46c3cf9f5f8c543d
Vaharsolta <vaha-22@mail.ru>
2017-05-30 08:40:28 +0300

    Реализовано добавление товаров без перезагрузки

--- web/js/common.js
+++ web/js/common.js
@@ -1,61 +1,92 @@
 $(document).ready(function(){
-
-
 	$( ".slider-slides" ).cycle({
         pager:'.slider-btn',
-        prev: '.prev',
-        next: '.next'
+        prev: '.next',
+        next: '.prev'
     });
 
-    $("a.zoom").prettyPhoto({
-        social_tools: ''
+
+
+
+
+
+function getCart(){
+    $.ajax({
+        url: '/cart/show',
+        type: 'GET',
+        success: function(res){
+            if(!res) alert('Ошибка!');
+            showCart(res);
+        },
+        error: function(){
+            alert('Error!');
+        }
     });
+    return false;
+}
 
-     $('.product figure .overlay a').hover(
-        function(){
-            $(this).stop().animate(
-                {backgroundPosition: "(0 -41px)"},
-                {duration:500}
-            )
+function showCart(cart) {
+    $('#cart .modal-body').html(cart);
+    $('#cart').modal();
+}
+
+$('#cart .modal-body').on('click', '.del-item', function () {
+    var id = $(this).data('id');
+    $.ajax({
+        url: '/cart/del-item',
+        data: {id: id},
+        type: 'GET',
+        success: function (res) {
+            if (!res) alert('Error!');
+            showCart(res);
+            $('.cart > ul > li').html(res);
         },
-        function(){
-            $(this).stop().animate(
-                {backgroundPosition: "(0 0)"},
-                {duration:500}
-            )
+        error: function () {
+            alert('Error!');
         }
-    );
+    });
+});
 
-    $('.sorting-bar .sorting-btn a').hover(
-        function(){
-            $(this).stop().animate(
-                {backgroundPosition: "(0 -29px)"},
-                {duration:500}
-            )
+
+function clearCart() {
+    $.ajax({
+        url: '/cart/clear',
+        type: 'GET',
+        success: function (res) {
+            if (!res) alert('Error!');
+            showCart(res);
         },
-        function(){
-            $(this).stop().animate(
-                {backgroundPosition: "(0 0)"},
-                {duration:500}
-            )
+        error: function () {
+            alert('Error!');
         }
-    );
+    });
+}
 
-    $('.detail .icon a').hover(
-        function(){
-            $(this).stop().animate(
-                {backgroundPosition: "(0 -42.3px)"},
-                {duration:500}
-            )
+    function setNewEntry(entry) {
+        $('.count-qty').html(entry);
+    }
+
+$('.add-to-cart').on('click', function (e) {
+    e.preventDefault(); //Отключаем переход по ссылке
+    var id = $(this).data('id');
+    $.ajax({
+        url: '/cart/add',
+        data: {id: id},
+        type: 'GET',
+        success: function (res) {
+            if (!res) alert('Error!');
+            //console.log(res);
+            showCart(res);
+            var cart = JSON.parse(res);
+            console.log(cart);
+
+            //setNewEntry(res.cart.sum);
+            //cart(res);
         },
-        function(){
-            $(this).stop().animate(
-                {backgroundPosition: "(0 0)"},
-                {duration:500}
-            )
+        error: function () {
+            alert('Error!');
         }
-    );
-
-
+    });
+});
 
 });
