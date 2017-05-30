1a7b9d92f66cf19435522d13ead66ff31e47a286
Vaharsolta <vaha-22@mail.ru>
2017-05-30 08:15:32 +0300

    Реализована модель по работе с корзиной

--- models/Cart.php
+++ models/Cart.php
@@ -1,15 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Vaharsolta
- * Date: 28.05.2017
- * Time: 17:24
- */
 
 namespace app\models;
 
+use yii\db\ActiveRecord;
 
-class Cart
+class Cart extends ActiveRecord
 {
+    public function addToCart($product, $qty = 1) {
+        if (isset($_SESSION['cart'][$product->id])) {
+            $_SESSION['cart'][$product->id]['qty'] += $qty;
+        } else {
+            $_SESSION['cart'][$product->id] = [
+                'qty' => $qty,
+                'name' => $product->name,
+                'price' => $product->price,
+                'img' => $product->image,
+            ];
+        }
 
+        $_SESSION['cart.qty'] = isset($_SESSION['cart.qty']) ? ($_SESSION['cart.qty']) + $qty : $qty;
+        $_SESSION['cart.sum'] = isset($_SESSION['cart.sum']) ? ($_SESSION['cart.sum']) + $qty * $product->price : $qty * $product->price;
+
+    }
+
+    public function recalc($id) {
+        if(!isset($_SESSION['cart'][$id])) return false;
+        $qtyMinus = $_SESSION['cart'][$id]['qty'];
+        $sumMinus = $_SESSION['cart'][$id]['qty'] * $_SESSION['cart'][$id]['price'];
+        $_SESSION['cart.qty'] -= $qtyMinus;
+        $_SESSION['cart.sum'] -= $sumMinus;
+        unset($_SESSION['cart'][$id]);
+    }
 }
