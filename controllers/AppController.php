868b8bafa1904ee5e8049b07f0d7efac3bebf611
Vaharsolta <vaha-22@mail.ru>
2017-05-30 08:14:40 +0300

    Вывод метатегов

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -8,7 +8,7 @@ class AppController extends Controller
 {
     protected function setMeta($title = null, $keywords = null, $description = null) {
         $this->view->title = $title;
-        $this->view->registerMetaTag(['name' => 'keywords', 'content' => "" . $keywords . ""]);
+        $this->view->registerMetaTag(['name' => 'keywords', 'content' => "" .$keywords . ""]);
         $this->view->registerMetaTag(['name' => 'description', 'content' => "" . $description . ""]);
     }
 }
