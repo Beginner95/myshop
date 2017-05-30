19aac631827b117139a9441869c9aebc402fd0e5
Vaharsolta <vaha-22@mail.ru>
2017-05-30 08:12:43 +0300

    Полностью реализована корзина на сессиях

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -1,15 +1,53 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Vaharsolta
- * Date: 28.05.2017
- * Time: 17:22
- */
 
 namespace app\controllers;
 
+use app\models\Cart;
+use app\models\Product;
+use Yii;
 
-class CartController
+class CartController extends AppController
 {
+    public function actionAdd() {
+        $id = Yii::$app->request->get('id');
+        $product = Product::findOne($id);
+        if (empty($product)) {
+            return false;
+        }
+        $session = Yii::$app->session;
+        $session->open();
+        $cart = new Cart();
+        $cart->addToCart($product);
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
 
+    }
+    
+    public function actionClear() {
+        $session = Yii::$app->session;
+        $session->open();
+        $session->remove('cart');
+        $session->remove('cart.qty');
+        $session->remove('cart.sum');
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+
+    public function actionDelItem() {
+        $id = Yii::$app->request->get('id');
+        $session =Yii::$app->session;
+        $session->open();
+        $cart = new Cart();
+        $cart->recalc($id);
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+
+    }
+
+    public function actionShow() {
+        $session =Yii::$app->session;
+        $session->open();
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
 }
