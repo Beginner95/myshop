cb046a0816701cbf9be8f738946f8711aad54b30
Vaharsolta <vaha-22@mail.ru>
2017-06-01 02:04:01 +0300

    Изменена БД

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=myshop',
+    'dsn' => 'mysql:host=localhost;dbname=my_shop',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
