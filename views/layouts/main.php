0e2626c07d8dc99421bae95d833666030747d61a
Vaharsolta <vaha-22@mail.ru>
2017-05-31 18:09:56 +0300

    Добавлена ссылка на содержание корзины

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -171,7 +171,9 @@ ltAppAsset::register($this);
             'header' => '<h2>Корзина</h2>',
             'id' => 'cart',
             'size' => 'modal-lg',
-            'footer' => '<button type="button" class="btn btn-default" data-dismiss="modal">Продолжить покупки</button> <button type="button" class="btn btn-success">Оформить заказ</button> <button type="button" class="btn btn-danger" onclick="clearCart()">Очистить корзину</button>'
+            'footer' => '<button type="button" class="btn btn-default" data-dismiss="modal">Продолжить покупки</button> 
+                         <a href="' . \yii\helpers\Url::to(['cart/view']) . '" class="btn btn-success">Оформить заказ</a> 
+                         <button type="button" class="btn btn-danger" onclick="clearCart()">Очистить корзину</button>'
         ]);
 
         \yii\bootstrap\Modal::end();
