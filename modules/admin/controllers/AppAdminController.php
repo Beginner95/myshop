c5a84b4090b14108837d0696cc7c17fba3b8edfc
Vaharsolta <vaha-22@mail.ru>
2017-06-02 20:55:44 +0300

    Создан метод поведения

--- modules/admin/controllers/AppAdminController.php
+++ modules/admin/controllers/AppAdminController.php
@@ -3,8 +3,23 @@
 namespace app\modules\admin\controllers;
 
 use yii\web\Controller;
+use yii\filters\AccessControl;
 
 class AppAdminController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
 
+                ],
+            ],
+        ];
+    }
 }
