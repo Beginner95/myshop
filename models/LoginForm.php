a136cb7d961e055719c8d8a4b787309f48bb488e
Vaharsolta <vaha-22@mail.ru>
2017-06-02 20:56:33 +0300

    Добавлен метод attributeLabels

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -35,6 +35,15 @@ class LoginForm extends Model
         ];
     }
 
+    public function attributeLabels()
+    {
+        return [
+            'username'   => 'Логин',
+            'password'   => 'Пароль',
+            'rememberMe' => 'Запомнить',
+        ];
+    }
+
     /**
      * Validates the password.
      * This method serves as the inline validation for password.
@@ -48,7 +57,7 @@ class LoginForm extends Model
             $user = $this->getUser();
 
             if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, 'Incorrect username or password.');
+                $this->addError($attribute, 'Логин/пароль введены не верно');
             }
         }
     }
