06302186117b8296c25740318d5e96f7ec4da657
Vaharsolta <vaha-22@mail.ru>
2017-06-04 00:19:23 +0300

    Создан каркас нового модуля

---
+++ modules/client/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\client;
+
+/**
+ * client module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\client\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
