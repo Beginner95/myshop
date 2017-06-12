4c1c703ffcbdb4dff5ca3d3d8c5b3155ab966c35
Vaharsolta <vaha-22@mail.ru>
2017-06-12 18:38:52 +0400

    Реализован шаблон для показа списка историй возврата

---
+++ modules/client/views/return/history.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use app\modules\client\controllers\OrderClientController;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Возвраты';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="order-index row">
+    <div class="col-md-3">
+        <h1><?= Html::encode($this->title) ?></h1>
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'columns' => [
+                [
+                    'attribute' => 'date_added',
+                    'value'     => function($data) {
+                        return '<a href="/client/return/view?id=' . $data->id . '">Заказ ' . OrderClientController::getDate($data->date_added) . '</a>';
+                    },
+                    'format' => 'html',
+                ],
+                'sum',
+                [
+                    'attribute' => 'status',
+                    'value'     => function($data) {
+                        return !$data->status ? '<span class="text-danger">В обработке</span>' : '<span class="text-success">Готов</span>';
+                    },
+                    'format'    => 'html',
+                ],
+            ],
+        ]); ?>
+    </div>
+    <?php if (!empty($model)) : ?>
+        <div class="col-md-9">
+
+        </div>
+    <?php endif; ?>
+</div>
