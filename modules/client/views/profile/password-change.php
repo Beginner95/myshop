4731df4eeaba7acbb7cb28f41dbfb653622617b3
Vaharsolta <vaha-22@mail.ru>
2017-06-17 17:40:00 +0400

    Изменен стиль

---
+++ modules/client/views/profile/password-change.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\client\models\User */
+
+$this->title = 'Смена пароля';
+$this->params['breadcrumbs'][] = ['label' => 'Профиль', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="container">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="user-form">
+
+        <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'currentPassword')->passwordInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'newPassword')->passwordInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'newPasswordRepeat')->passwordInput(['maxlength' => true]) ?>
+
+        <div class="form-group">
+            <?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
+    </div>
+
+</div>
