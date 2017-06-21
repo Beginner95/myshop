657911a4dec406089c6e0601bd75b431355391a6
Vaharsolta <vaha-22@mail.ru>
2017-06-21 18:43:59 +0300

    Описаны столбцы для импорта из Excel

---
+++ modules/admin/models/Price.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\modules\admin\models;
+
+
+class Price
+{
+    public $cells = [
+        'A' => 'name',
+        'B' => 'model',
+        'C' => 'price',
+        'D' => 'wholesale_price',
+        'E' => 'content',
+        'F' => 'status',
+        'G' => 'sale',
+        'H' => 'new',
+        'I' => 'hit',
+        'J' => 'keywords',
+        'K' => 'description',
+    ];
+}
