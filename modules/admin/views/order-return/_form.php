c468d6d63610677f85f3289dc712dfd0d5c2cd0a
Vaharsolta <vaha-22@mail.ru>
2017-06-30 04:28:18 +0300

    Добавлено скрытое поле в которой находится ИД клиента

--- modules/admin/views/order-return/_form.php
+++ modules/admin/views/order-return/_form.php
@@ -22,6 +22,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'status')->dropDownList([ '0' => 'Не подтвержден', '1' => 'Подтвержден',]) ?>
 
+    <?= $form->field($model, 'user_id')->hiddenInput() ?>
+
     <div class="form-group">
         <?= Html::submitButton('Обновить', ['class' => 'btn btn-primary']) ?>
     </div>
