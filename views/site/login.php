209c2299c348bc9b0f3cf3839223f416a807ce94
Vaharsolta <vaha-22@mail.ru>
2017-07-01 21:22:28 +0300

    Добавлен вывод сообщений

--- views/site/login.php
+++ views/site/login.php
@@ -11,6 +11,12 @@ $this->title = 'Вход';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="container site-login">
+    <?php if (Yii::$app->session->hasFlash('success')) : ?>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo Yii::$app->session->getFlash('success'); ?>
+        </div>
+    <?php endif; ?>
     <h1><?= Html::encode($this->title) ?></h1>
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
