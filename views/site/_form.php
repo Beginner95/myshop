f5d60a4a484fff74e258370be080376a1c530991
Vaharsolta <vaha-22@mail.ru>
2017-07-01 21:21:15 +0300

    Добавлена форма для запроса на регистрацию

---
+++ views/site/_form.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\User */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'firstName')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'secondName')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'lastName')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Создать', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
