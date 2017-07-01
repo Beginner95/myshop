155b3ea15c8d58b60a50b3afa1a09cfa5a3ee350
Vaharsolta <vaha-22@mail.ru>
2017-07-01 11:13:20 +0300

    Реализован функционал Один заказ на 24 часа, сколько бы не было повторных заказов в день

--- modules/client/controllers/CartController.php
+++ modules/client/controllers/CartController.php
@@ -2,6 +2,7 @@
 
 namespace app\modules\client\controllers;
 
+use app\models\Order;
 use app\modules\client\models\Cart;
 use app\modules\client\models\Product;
 use app\modules\client\models\Delivery;
@@ -68,9 +69,19 @@ class CartController extends AppClientController
         $session = Yii::$app->session;
         $session->open();
         $this->setMeta('Корзина');
-        $order = new OrderClient();
 
+        $order = new OrderClient();
         if ($order->load(Yii::$app->request->post())) {
+
+            $items = OrderClient::find()
+                ->select('id, date_added')
+                ->all();
+            foreach ($items as $item) {
+                if (OrderClientController::getDate($item->date_added) == date('d-m-Y')) {
+                    return $this->updateOrder($item->id);
+                }
+            }
+
             $order->qty = $session['cart.qty'];
             $order->sum = $session['cart.sum'] * ((100 - Yii::$app->user->identity->discount) / 100);
             $order->client_id = Yii::$app->user->identity->id;
@@ -106,18 +117,67 @@ class CartController extends AppClientController
         return $this->render('view', compact('session', 'order', 'fio', 'delivery'));
     }
 
+    protected function updateOrder($id)
+    {
+        $session = Yii::$app->session;
+        $session->open();
+
+        $order = OrderClient::findOne(['id' => $id]);
+        $order->status = '0';
+        $order->qty += $session['cart.qty'];
+        $order->sum += $session['cart.sum'] * ((100 - Yii::$app->user->identity->discount) / 100);
+        $order->client_id = Yii::$app->user->identity->id;
+
+        if ($order->save()) {
+            $this->saveOrderItemsClient($session['cart'], $order->id);
+            Yii::$app->session->setFlash('success', 'Ваш заказ принят. Менеджер вскоре свяжется с Вами.');
+
+            Yii::$app->mailer->compose('order', ['session' => $session])
+                ->setFrom(['test@mail.ru' => 'MyShop'])
+                ->setTo($order->email)
+                ->setSubject('Заказ')
+                ->send();
+
+            $session->remove('cart');
+            $session->remove('cart.qty');
+            $session->remove('cart.sum');
+            return $this->refresh();
+        } else {
+            Yii::$app->session->setFlash('error', 'Ошибка оформления заказа.');
+        }
+
+        $delivery = Delivery::find()->all();
+        $fio = [
+            'firstName' => Yii::$app->user->identity->firstName,
+            'secondName' => Yii::$app->user->identity->secondName,
+            'lastName' => Yii::$app->user->identity->lastName,
+            'email' => Yii::$app->user->identity->email,
+            'phone' => Yii::$app->user->identity->phone,
+            'address' => Yii::$app->user->identity->address,
+        ];
+        return $this->render('view', compact('session', 'order', 'fio', 'delivery'));
+
+    }
+
     protected function saveOrderItemsClient($items, $order_id)
     {
         foreach ($items as $id => $item) {
-            $order_items = new OrderItemsClient();
-            $order_items->order_client_id = $order_id;
-            $order_items->product_id = $id;
-            $order_items->client_id = Yii::$app->user->identity->id;
-            $order_items->name = $item['name'];
-            $order_items->price = $item['price'] * ((100 - Yii::$app->user->identity->discount) / 100);
-            $order_items->qty_item = $item['qty'];
-            $order_items->sum_item = $item['qty'] * $item['price'] * ((100 - Yii::$app->user->identity->discount) / 100);
-            $order_items->save();
+            $model = OrderItemsClient::findOne(['product_id' => $id]);
+            if (null !== $model) {
+                $model->qty_item += $item['qty'];
+                $model->sum_item += $item['qty'] * $item['price'] * ((100 - Yii::$app->user->identity->discount) / 100);
+                $model->save();
+            } else {
+                $order_items = new OrderItemsClient();
+                $order_items->order_client_id = $order_id;
+                $order_items->product_id = $id;
+                $order_items->client_id = Yii::$app->user->identity->id;
+                $order_items->name = $item['name'];
+                $order_items->price = $item['price'] * ((100 - Yii::$app->user->identity->discount) / 100);
+                $order_items->qty_item = $item['qty'];
+                $order_items->sum_item = $item['qty'] * $item['price'] * ((100 - Yii::$app->user->identity->discount) / 100);
+                $order_items->save();
+            }
         }
     }
 }
