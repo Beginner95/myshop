fe887354088e19dc2bbe754c978376e077d348ae
Vaharsolta <vaha-22@mail.ru>
2017-07-02 13:08:27 +0300

    Добавлена форма обнавления настроек сайта

---
+++ modules/admin/views/site-settings/_form.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\SiteSettings */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="site-settings-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'logotip')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'name_company')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'slogan')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'usd')->textInput() ?>
+
+    <?= $form->field($model, 'rub')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Обновить', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
