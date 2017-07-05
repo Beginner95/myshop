5d7d8468e07c6c14343c1f7d53b7cc798a31e0bb
Vaharsolta <vaha-22@mail.ru>
2017-07-05 06:33:14 +0300

    Название кнопок переведено на русский язык

--- modules/admin/views/delivery/_form.php
+++ modules/admin/views/delivery/_form.php
@@ -21,7 +21,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'Создать' : 'Обновить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
