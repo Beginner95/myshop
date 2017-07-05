32b864382cf9b8c8219e34c6f0abb4b80aa9503f
Vaharsolta <vaha-22@mail.ru>
2017-07-05 06:50:12 +0300

    Удалено лишнее поле с id

--- modules/admin/views/delivery/_form.php
+++ modules/admin/views/delivery/_form.php
@@ -12,8 +12,6 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id')->textInput() ?>
-
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'cost')->textInput(['maxlength' => true]) ?>
