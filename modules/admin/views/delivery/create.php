1449e9a1218716d68edfd76bd07da99b4eb766e0
Vaharsolta <vaha-22@mail.ru>
2017-07-05 06:30:08 +0300

    Добавлен шаблон добавления способа доставки

---
+++ modules/admin/views/delivery/create.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\Delivery */
+
+$this->title = 'Create Delivery';
+$this->params['breadcrumbs'][] = ['label' => 'Deliveries', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="delivery-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
