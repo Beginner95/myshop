bcc03cf066dc9b11d7b8f34c4ccd48283263b390
Vaharsolta <vaha-22@mail.ru>
2017-07-07 11:25:09 +0300

    Реализовано вывод сообщений при удалении клиента

--- modules/admin/views/user/index.php
+++ modules/admin/views/user/index.php
@@ -16,6 +16,18 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         <?= Html::a('Добавить клиента', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
+    <?php if (Yii::$app->session->hasFlash('success')) : ?>
+        <div class="alert alert-success alert-dismissable" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo Yii::$app->session->hasFlash('success'); ?>
+        </div>
+    <?php endif; ?>
+    <?php if (Yii::$app->session->hasFlash('error')) : ?>
+        <div class="alert alert-danger alert-dismissable" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo Yii::$app->session->getFlash('error'); ?>
+        </div>
+    <?php endif; ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
